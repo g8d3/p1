8f10cbdc8d5fad14f682e6433dddc7ae5a69f23a
qwe@qwe.qwe <qwe@qwe.qwe>
2025-05-14 20:40:24 +0000

    May 14, 2025, 3:40 PM

--- s3-trade/notte_test.py
+++ s3-trade/notte_test.py
@@ -1,4 +1,4 @@
 import notte
 
 agi = notte.Agent(reasoning_model="gemini/gemini-2.0-flash", max_steps=5)
-agi.run(task="doom scroll cat memes on google images")
+agi.run(task="go to https://gmgn.ai/trade?chain=sol and find which url is browser using to fetch items on displayed table")

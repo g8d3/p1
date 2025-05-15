c172b575df21ac0e20c420ca46817fb7a8032e8d
qwe@qwe.qwe <qwe@qwe.qwe>
2025-05-15 03:06:35 +0000

    May 14, 2025, 10:06 PM

--- s3-trade/notte_test.py
+++ s3-trade/notte_test.py
@@ -1,4 +1,4 @@
 import notte
 
 agi = notte.Agent(reasoning_model="gemini/gemini-2.0-flash", max_steps=5)
-agi.run(task="go to https://gmgn.ai/trade?chain=sol and find which url is browser using to fetch items on displayed table")
+agi.run(task="go to https://gmgn.ai/trade?chain=sol and find which url is browser using to fetch items on displayed table, do not click away from that url, check network tab on developers tools and look for XHR requests")

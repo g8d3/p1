39aae371a0664dbf96c47573e01a4828d071f899
PJ <pj@gmail.com>
2025-09-29 01:01:55 +0000

    Sep 28, 2025, 8:01 PM

---
+++ s4-trade/t7/ext-tradr/handlers/saveSettings.js
@@ -0,0 +1,6 @@
+// Handler for saving settings
+function saveSettings(message, sender, sendResponse) {
+  // Get all fields from config.ui.popup.fields
+  // Save to chrome.storage.sync
+  // Show message
+}

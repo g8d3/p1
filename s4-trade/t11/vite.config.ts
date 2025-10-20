d311a0d214ad61b0b9c333c46d74f9850fa3d418
Your Name <you@example.com>
2025-10-20 17:11:51 -0500

    Remove unnecessary polyfills since using ethers instead of OKX

--- s4-trade/t11/vite.config.ts
+++ s4-trade/t11/vite.config.ts
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), nodePolyfills({ include: ['buffer'], globals: { Buffer: true } })],
+  plugins: [react()],
   define: {
     global: 'globalThis',
-  },
-  optimizeDeps: {
-    include: ['@okxweb3/coin-ethereum', '@okxweb3/crypto-lib', 'buffer'],
   },
 })
